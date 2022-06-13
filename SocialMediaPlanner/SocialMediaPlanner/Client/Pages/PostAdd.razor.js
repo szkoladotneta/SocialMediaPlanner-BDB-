50b378eebe05ca23557cae4703307b2ae89bb7ff
szkoladotneta <kajetan@szkoladotneta.pl>
2022-06-13 23:29:42 +0200

    modul9

---
+++ SocialMediaPlanner/SocialMediaPlanner/Client/Pages/PostAdd.razor.js
@@ -0,0 +1,6 @@
+﻿export function AddTextToTextbox(message, dotNetObject) {
+    var title = document.getElementById("title");
+    title.value = message;
+
+    dotNetObject.invokeMethodAsync("AddText");
+}
